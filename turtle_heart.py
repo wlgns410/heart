1d9a07d5aa8e9fb8a2d58c318df146b660cf0f22
wlgns410 <81137234+wlgns410@users.noreply.github.com>
2021-05-20 12:32:57 +0900

    Add files via upload

---
+++ turtle_heart.py
@@ -0,0 +1,16 @@
+import turtle as t
+
+t.bgcolor('#FFDCDC')
+t.color('#E478E4')
+t.shape('turtle')
+t.begin_fill()
+t.left(60)
+t.forward(200)
+t.circle(50, 200)
+t.left(200)
+t.circle(50, 200)
+t.forward(200)
+t.end_fill()
+
+t.hideturtle()
+t.mainloop()
